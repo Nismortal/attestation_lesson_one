c098dbbd0eb8b298f979a5474e374de824a699cd
Nismortal <91359321+Nismortal@users.noreply.github.com>
2021-10-11 22:09:16 +0300

    done task1, task2

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        Timer timer = new Timer();
+
+        Runnable timeOne = new Message(7, timer);
+        Runnable timeTwo = new Message(5, timer);
+        //Runnable timeThree = new Message(1, timer);
+
+        new Thread(timeOne, "timeOne").start();
+        new Thread(timeTwo, "timeTwo").start();
+        //new Thread(timeThree, "timeThree").start();
+        timer.timeCount((Message) timeOne, (Message) timeTwo, 30);
+    }
+
+}
